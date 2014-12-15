d7182b45602b59ce74637e936deee796eaa2084d
Andrei F <luxneb@gmail.com>
2014-12-15 22:41:31 +0400

    devfreq: Add generic voltage control
    Conflicts:
    	drivers/devfreq/devfreq.c

--- drivers/devfreq/devfreq.c
+++ drivers/devfreq/devfreq.c
@@ -23,10 +23,19 @@
 #include <linux/devfreq.h>
 #include <linux/workqueue.h>
 #include <linux/platform_device.h>
+#include <linux/sysfs_helpers.h>
 #include <linux/list.h>
 #include <linux/printk.h>
 #include <linux/hrtimer.h>
 #include "governor.h"
+
+#ifdef CONFIG_SOC_EXYNOS7420
+#define DF_MAX_VOLT		900000
+#define DF_MIN_VOLT		500000
+#define DF_VOLT_STEP		6250
+#else
+#error "Please define core voltage ranges for current SoC."
+#endif
 
 static struct class *devfreq_class;
 
@@ -1080,6 +1089,7 @@ static ssize_t time_in_state_show(struct device *dev,
 	}
 	return len;
 }
+<<<<<<< HEAD
 static DEVICE_ATTR_RO(time_in_state);
 
 static struct attribute *devfreq_attrs[] = {
@@ -1094,6 +1104,87 @@ static struct attribute *devfreq_attrs[] = {
 	&dev_attr_trans_stat.attr,
 	&dev_attr_time_in_state.attr,
 	NULL,
+=======
+
+
+static ssize_t store_volt_table(struct device *d, struct device_attribute *attr,
+			      const char *buf, size_t count)
+{
+	struct devfreq *df = to_devfreq(d);
+	struct device *dev = df->dev.parent;
+	struct device_opp *dev_opp = find_device_opp(dev);
+	struct opp *temp_opp;
+	int u[15];
+	int rest, t, i = 0;
+
+	if ((t = read_into((int*)&u, 15, buf, count)) < 0)
+		return -EINVAL;
+
+	mutex_lock(&df->lock);
+	if (t == 2 && 15 != 2) {
+		temp_opp = opp_find_freq_exact(dev, u[0], true);
+		if(IS_ERR(temp_opp))
+			return -EINVAL;
+
+		if ((rest = (u[1] % DF_VOLT_STEP)) != 0) 
+			u[1] += DF_VOLT_STEP - rest;
+		
+		sanitize_min_max(u[1], DF_MIN_VOLT, DF_MAX_VOLT);
+		temp_opp->u_volt = u[1];
+	} else {
+		list_for_each_entry_rcu(temp_opp, &dev_opp->opp_list, node) {
+			if (temp_opp->available) {
+				if ((rest = (u[i] % DF_VOLT_STEP)) != 0) 
+					u[i] += DF_VOLT_STEP - rest;
+				
+				sanitize_min_max(u[i], DF_MIN_VOLT, DF_MAX_VOLT);
+				temp_opp->u_volt = u[i++];
+			}
+		}
+	}
+
+	mutex_unlock(&df->lock);
+
+	return count;
+}
+
+static ssize_t show_volt_table(struct device *d, struct device_attribute *attr,
+			     char *buf)
+{
+	struct devfreq *df = to_devfreq(d);
+	struct device *dev = df->dev.parent;
+	struct device_opp *dev_opp = find_device_opp(dev);
+	struct opp *temp_opp;
+	int len = 0;
+
+	if (IS_ERR_OR_NULL(dev_opp))
+		return -EINVAL;
+
+	list_for_each_entry_rcu(temp_opp, &dev_opp->opp_list, node) {
+		if (temp_opp->available)
+			len += sprintf(buf + len, "%lu %lu\n",
+					opp_get_freq(temp_opp),
+					opp_get_voltage(temp_opp));
+	}
+
+	return len;
+}
+
+static struct device_attribute devfreq_attrs[] = {
+	__ATTR(governor, S_IRUGO | S_IWUSR, show_governor, store_governor),
+	__ATTR(available_governors, S_IRUGO, show_available_governors, NULL),
+	__ATTR(cur_freq, S_IRUGO, show_freq, NULL),
+	__ATTR(available_frequencies, S_IRUGO, show_available_freqs, NULL),
+	__ATTR(target_freq, S_IRUGO, show_target_freq, NULL),
+	__ATTR(polling_interval, S_IRUGO | S_IWUSR, show_polling_interval,
+	       store_polling_interval),
+	__ATTR(min_freq, S_IRUGO | S_IWUSR, show_min_freq, store_min_freq),
+	__ATTR(max_freq, S_IRUGO | S_IWUSR, show_max_freq, store_max_freq),
+	__ATTR(trans_stat, S_IRUGO, show_trans_table, NULL),
+	__ATTR(time_in_state_raw, S_IRUGO, show_time_in_state, NULL),
+	__ATTR(volt_table, S_IRUGO | S_IWUSR, show_volt_table, store_volt_table),
+	{ },
+>>>>>>> b6a9e2b... devfreq: Add generic voltage control
 };
 ATTRIBUTE_GROUPS(devfreq);
 
