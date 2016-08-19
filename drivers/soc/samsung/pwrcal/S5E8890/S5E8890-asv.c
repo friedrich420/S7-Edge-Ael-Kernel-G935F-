dad099de6fb9d7a6fd9195640258015b72ddb787
EchoeGit <kelpidorou@yahoo.com>
2016-08-19 11:48:44 +0300

    Revert "update gpu drivers , ael configs and fixes"
    This reverts commit d5e5459f84e7ad6df988aa76d8f92aa222ac627c.

--- drivers/soc/samsung/pwrcal/S5E8890/S5E8890-asv.c
+++ drivers/soc/samsung/pwrcal/S5E8890/S5E8890-asv.c
@@ -153,8 +153,6 @@ static unsigned int cam_subgrp_index = 256;
 static unsigned int disp_subgrp_index = 256;
 static unsigned int g3dm_subgrp_index = 256;
 
-static unsigned int asv_undervoltage = 6250 * 4;
-
 static unsigned int big_ssa1_table[8];
 static unsigned int little_ssa1_table[8];
 static unsigned int g3d_ssa1_table[8];
@@ -578,18 +576,8 @@ static int dvfsbig_get_asv_table(unsigned int *table)
 
 	max_lv = asv_dvfs_big->table->num_of_lv;
 
-	for (lv = 0; lv < max_lv; lv++) {
-		table[lv] = get_asv_voltage(cal_asv_dvfs_big, lv) - asv_undervoltage;
-		if (lv > 18) {
-                	if (table[lv-1] - (6250 * 2) > 500000)
-				table[lv] = table[lv-1] - (6250 * 2);
-			else
-				table[lv] = 500000;
-		}
-	}
-
-	for (lv = 2; lv >= 0; lv--)
-		table[lv] = table[lv+1] + (6250 * 10);
+	for (lv = 0; lv < max_lv; lv++)
+		table[lv] = get_asv_voltage(cal_asv_dvfs_big, lv);
 
 	return max_lv;
 }
@@ -600,18 +588,8 @@ static int dvfslittle_get_asv_table(unsigned int *table)
 
 	max_lv = asv_dvfs_little->table->num_of_lv;
 
-	for (lv = 0; lv < max_lv; lv++) {
-		table[lv] = get_asv_voltage(cal_asv_dvfs_little, lv) - asv_undervoltage;
-		if (lv > 14) {
-                	if (table[lv-1] - (6250 * 2) > 500000)
-				table[lv] = table[lv-1] - (6250 * 2);
-			else
-				table[lv] = 500000;
-		}
-	}
-
-	for (lv = 3; lv >= 0; lv--)
-		table[lv] = table[lv+1] + (6250 * 10);
+	for (lv = 0; lv < max_lv; lv++)
+		table[lv] = get_asv_voltage(cal_asv_dvfs_little, lv);
 
 	return max_lv;
 }
@@ -622,20 +600,8 @@ static int dvfsg3d_get_asv_table(unsigned int *table)
 
 	max_lv = asv_dvfs_g3d->table->num_of_lv;
 
-	for (lv = 0; lv < max_lv; lv++) {
-		table[lv] = get_asv_voltage(cal_asv_dvfs_g3d, lv) - asv_undervoltage;
-		if (lv > 8) {
-                	if (table[lv-1] - (6250 * 2) > 500000)
-				table[lv] = table[lv-1] - (6250 * 4);
-			else
-				table[lv] = 500000;
-		}
-	}
-
-	if (table[1] + (6250 * 8) < 850000)
-	        table[0] = table[1] + (6250 * 8);
-	else
-	        table[0] = 850000;
+	for (lv = 0; lv < max_lv; lv++)
+		table[lv] = get_asv_voltage(cal_asv_dvfs_g3d, lv);
 
 	return max_lv;
 }
@@ -647,7 +613,7 @@ static int dvfsmif_get_asv_table(unsigned int *table)
 	max_lv = asv_dvfs_mif->table->num_of_lv;
 
 	for (lv = 0; lv < max_lv; lv++)
-		table[lv] = get_asv_voltage(cal_asv_dvfs_mif, lv) - asv_undervoltage;
+		table[lv] = get_asv_voltage(cal_asv_dvfs_mif, lv);
 
 	return max_lv;
 }
@@ -659,7 +625,7 @@ static int dvfsint_get_asv_table(unsigned int *table)
 	max_lv = asv_dvfs_int->table->num_of_lv;
 
 	for (lv = 0; lv < max_lv; lv++)
-		table[lv] = get_asv_voltage(cal_asv_dvfs_int, lv) - asv_undervoltage;
+		table[lv] = get_asv_voltage(cal_asv_dvfs_int, lv);
 
 	return max_lv;
 }
@@ -671,7 +637,7 @@ static int dvfscam_get_asv_table(unsigned int *table)
 	max_lv = asv_dvfs_cam->table->num_of_lv;
 
 	for (lv = 0; lv < max_lv; lv++)
-		table[lv] = get_asv_voltage(cal_asv_dvfs_cam, lv) - asv_undervoltage;
+		table[lv] = get_asv_voltage(cal_asv_dvfs_cam, lv);
 
 	return max_lv;
 }
@@ -683,7 +649,7 @@ static int dvfsdisp_get_asv_table(unsigned int *table)
 	max_lv = asv_dvfs_disp->table->num_of_lv;
 
 	for (lv = 0; lv < max_lv; lv++)
-		table[lv] = get_asv_voltage(cal_asv_dvfs_disp, lv) - asv_undervoltage;
+		table[lv] = get_asv_voltage(cal_asv_dvfs_disp, lv);
 
 	return max_lv;
 }
@@ -695,7 +661,7 @@ static int dvsg3dm_get_asv_table(unsigned int *table)
 	max_lv = asv_dvs_g3dm->table->num_of_lv;
 
 	for (lv = 0; lv < max_lv; lv++)
-		table[lv] = get_asv_voltage(cal_asv_dvs_g3dm, lv) - asv_undervoltage;
+		table[lv] = get_asv_voltage(cal_asv_dvs_g3dm, lv);
 
 	return max_lv;
 }
